ab5f395ef0fa94b80daff3134ccf7e1abbeb88dc
AchFaisol <achfaisollllll@gmail.com>
2025-04-13 16:03:37 +0700

    230441100181-MELATI ULFA WIJAYA-KAK RAKHA EXAM-LATIHAN MODUL 1

--- tugas-dart/pember-dart-code-submission/bin/exam1/exam1.dart
+++ tugas-dart/pember-dart-code-submission/bin/exam1/exam1.dart
@@ -1,9 +1,9 @@
 dynamic studentInfo() {
   // TODO 1
 
-  var name;
-  var favNumber;
-  var isPraktikan;
+  var name = "Melati Ulfa Wijaya";
+  var favNumber = 7;
+  final bool isPraktikan = true;
 
   // End of TODO 1
   return [name, favNumber, isPraktikan];
@@ -16,8 +16,8 @@ dynamic circleArea(num r) {
     const double pi = 3.1415926535897932; //π sama dengan library dart.math;
 
     // TODO 2
-
-    return 0;
+    double area = pi * r * r;
+    return area;
 
     // End of TODO 2
   }
@@ -25,8 +25,15 @@ dynamic circleArea(num r) {
 
 int? parseAndAddOne(String? input) {
   // TODO 3
+  if (input == null) {
+    return null;
+  }
 
-  return 0;
+  int? parsedNumber = int.tryParse(input);
+  if (parsedNumber == null) {
+    throw Exception("Input harus berupa angka");
+  }
+  return parsedNumber + 1;
 
   // End of TODO 3
 }
